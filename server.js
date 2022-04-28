685b971bcce01b44620ad13bd2d6a299513ac367
abderrahmane <abderrahamane7@gmail.com>
2022-04-28 19:12:08 +0000

    added database connection and implemented auth routes

--- server.js
+++ server.js
@@ -2,14 +2,33 @@ require("dotenv").config()
 
 const express = require("express")
 const app = express()
+const authRouter = require("./routes/auth")
+
+// ? env variables
 const port = process.env.PORT || 3001
 const host = process.env.HOST || "localhost"
+const mongoose = require("mongoose")
+
+// ? connection to the database
+mongoose.connect(process.env.MONGODB_URI)
+
+// ? to ensure the connection to the database
+mongoose.connection.once("open", () => {
+  console.log("connected to the database")
+})
+
+// ? to parse incoming requests with json payload
 app.use(express.json())
+
+// * different routes
 
 app.get("/", (req, res) => {
   res.json({ message: "home route" })
 })
 
+app.use("/auth", authRouter)
+
+// ? to catch undefined routes
 app.get("*", (req, res) => {
   res.status(404).json({ message: "not found" })
 })
