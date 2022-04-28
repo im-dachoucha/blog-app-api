6833899fcd44c4dbc06340a756bf66e9423f6930
abderrahmane <abderrahamane7@gmail.com>
2022-04-28 22:45:51 +0000

    added register functionality to the auth controller

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,8 +1,18 @@
+const UserModel = require("../models/User")
+
 module.exports = {
   login: (req, res) => {
     res.json({ message: "login route" })
   },
-  register: (req, res) => {
-    res.json({ message: "register route" })
+
+  register: async (req, res) => {
+    try {
+      const { username, email, password } = req.body
+      const newUser = new UserModel({ username, email, password })
+      await newUser.save()
+      res.status(201).json({ user: newUser })
+    } catch (e) {
+      res.status(400).json({ message: e.message })
+    }
   },
 }
