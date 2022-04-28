9b06b819baad47c906bc00117599ee3763ac9b3d
abderrahmane <abderrahamane7@gmail.com>
2022-04-28 19:09:59 +0000

    added auth routes

---
+++ routes/auth.js
@@ -0,0 +1,8 @@
+const router = require("express").Router()
+const AuthController = require("../controllers/AuthController")
+
+router.post("/login", AuthController.login)
+
+router.post("/register", AuthController.register)
+
+module.exports = router
