4a99cc05397cfebeb71df18fbd94505deb39a3dc
abderrahmane <abderrahamane7@gmail.com>
2022-04-29 01:52:51 +0000

    added login functionality to the auth controller

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,8 +1,18 @@
 const UserModel = require("../models/User")
 
 module.exports = {
-  login: (req, res) => {
-    res.json({ message: "login route" })
+  login: async (req, res) => {
+    try {
+      const { email, password } = req.body
+      const user = await UserModel.findOne({ email, password })
+      if (!user)
+        return res
+          .status(404)
+          .json({ user, message: "no user found with the given credentials!!" })
+      res.json({ user, message: "success" })
+    } catch (e) {
+      res.status(400).json({ message: e.message })
+    }
   },
 
   register: async (req, res) => {
