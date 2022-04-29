71a914cfb44eaaabd2d7778dda331e4464f8bfc6
abderrahmane <abderrahamane7@gmail.com>
2022-04-29 14:29:23 +0000

    added unique property to the username and email keys

--- models/User.js
+++ models/User.js
@@ -4,10 +4,12 @@ const UserSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
+    unique: true,
   },
   email: {
     type: String,
     required: true,
+    unique: true,
   },
   password: {
     type: String,
