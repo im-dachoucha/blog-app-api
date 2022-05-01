6aa81723f99f4d2e8896499bc35f546cc91532a8
abderrahmane <abderrahamane7@gmail.com>
2022-05-01 14:35:08 +0000

    implemented bcrypt

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,14 +1,16 @@
 const UserModel = require("../models/User")
+const bcrypt = require("bcrypt")
 
 module.exports = {
   login: async (req, res) => {
     try {
       const { email, password } = req.body
-      const user = await UserModel.findOne({ email, password })
-      if (!user)
+      const user = await UserModel.findOne({ email })
+      if (!user || !(await bcrypt.compare(password, user.password)))
         return res
           .status(404)
-          .json({ user, message: "no user found with the given credentials!!" })
+          .json({ message: "no user found with the given credentials!!" })
+
       res.json({ user, message: "success" })
     } catch (e) {
       res.status(400).json({ message: e.message })
@@ -18,7 +20,13 @@ module.exports = {
   register: async (req, res) => {
     try {
       const { username, email, password } = req.body
-      const newUser = new UserModel({ username, email, password })
+      const salt = await bcrypt.genSalt()
+      const hashedPassword = await bcrypt.hash(password, salt)
+      const newUser = new UserModel({
+        username,
+        email,
+        password: hashedPassword,
+      })
       await newUser.save()
       res.status(201).json({ user: newUser })
     } catch (e) {
